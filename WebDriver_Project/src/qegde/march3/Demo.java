5dc3fd0cf63b6efcb46d2b56fe2eb9cdf740eee7
Pratiksha Golait <Pratiksha Golait@DESKTOP-SLGG1HT>
2025-05-16 23:02:28 +0530

    adding demo class

---
+++ WebDriver_Project/src/qegde/march3/Demo.java
@@ -0,0 +1,18 @@
+package qegde.march3;
+//Git hub commit demo
+public class Demo {
+	
+	private void pvm1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void pvm2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void pvm3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
